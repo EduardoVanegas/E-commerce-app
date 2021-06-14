84c04e21032fc86ada97ca1f93437c56d702b0c4
EduardoVanegas <48812625+EduardoVanegas@users.noreply.github.com>
2021-06-14 13:31:36 -0500

    node

--- src/data/cart.ts
+++ src/data/cart.ts
@@ -4,12 +4,12 @@ export default [
     quantity: 1,
     item: {
       id: '1',
-      title: "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
-      image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleanarchitecture.jpg',
-      avgRating: 4.2,
-      ratings: 1325,
-      price: 20.98,
-      oldPrice: 24.06,
+      title: "Cesto sonorense",
+      image: 'http://lutisuc.org/images/catalogo%20WEB/SERIS/CESTERIA/PDS10c.jpg',
+      avgRating: 4,
+      ratings: 1300,
+      price: 250,
+      oldPrice: 34.06,
     }
   },
   {
@@ -17,12 +17,12 @@ export default [
     quantity: 2,
     item: {
       id: '2',
-      title: "Clean Code: A Handbook of Agile Software Craftsmanship",
-      image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleancode.jpg',
-      avgRating: 4.8,
-      ratings: 2989,
-      price: 32.98,
-      oldPrice: 34.06,
+      title: "Ceramica de Mata Ortiz",
+      image: 'https://referente.mx/public/assets/2020/may/21/obj18067/AibA9vsu3d.jpg',
+      avgRating: 5,
+      ratings: 9000,
+      price: 3000,
+      oldPrice: 120.06,
     }
   },
   {
@@ -31,11 +31,11 @@ export default [
     option: 'Space Grey',
     item: {
       id: '5',
-      title: "Mouse Havit Mechanical Keyboard Wired 89 Keys Gaming Keyboard",
-      image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/mouse2.jpg',
-      avgRating: 4.8,
-      ratings: 2989,
-      price: 99.98,
+      title: "Arte Floral",
+      image: 'https://aripo.oaxaca.gob.mx/wp-content/uploads/2021/06/IMG_8243-300x300.png',
+      avgRating: 3,
+      ratings: 4100,
+      price: 200,
       oldPrice: 120.06,
     }
   },
